becb90fa0c7d5da797f0c90fc7d53a686a733078
smool <igorfb4@hotmail.com>
2025-11-12 16:26:18 -0300

    Update __ex__postcard__.py

--- examples/__ex__postcard__.py
+++ examples/__ex__postcard__.py
@@ -5,13 +5,13 @@ import os
 set_start_first_use_config()
 
 #define locapath
-_path_local = os.getcwd()
-_path_lib = f'{path_local}/_lib'
-_path_out = f'{path_lib}/_out'
-_path_fonts = f'{path_lib}/_fonts'
+path_local = os.getcwd()
+path_lib = f'{path_local}/_lib'
+path_out = f'{path_lib}/_out'
+path_fonts = f'{path_lib}/_fonts'
 
 # Set initial starting image
-set_init(f'{path_lib}/_out/postcard.jpg')
+set_init(f'{path_local}/_lib/postcard/postcard.jpg')
 
 #define background color RGB like 'blank' or (255, 255, 255)
 _rgb = 'BLANK' 
@@ -20,11 +20,11 @@ _rgb = 'BLANK'
 set_board(800, 600, 'white')  # white background blank
 
 # Add a portrait background (by style/keyword)
-_background = f'{path_lib}/postcard/postcard.jpg'
+_background = f'{path_local}/_lib/postcard/postcard.jpg'
 _buff_w = set_background(0, "NIL", _background, 0) # #use 0 - to websearch is not allowed - [1 - when is decrapetd]
 
 # Place a character and remove its background
-_thumbnail = f'{path_lib}/postcard/image_postcal_01.jpg'
+_thumbnail = f'{path_local}/_lib/postcard/image_postcal_01.jpg'
 _postal = resize_picture(_thumbnail,(240, 240))
 print(f'--------------------------{_postal}---------------------')
 _logo =  add_picture(_postal,50,50)
@@ -36,6 +36,6 @@ BECAUSE I MISS YOU
 PS:BARINSKY"""
 
 # Add text overlay
-set_text(1,_gui,_text,60,300,(0, 0, 0),f'{path_lib}/_fonts/Montserrat-Regular.ttf',22)
+set_text(1,_gui,_text,60,300,(0, 0, 0),f'{path_local}/_lib/_fonts/Montserrat-Regular.ttf',22)
 
 set_save()
